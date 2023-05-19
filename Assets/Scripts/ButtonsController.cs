ca9d36526ae472b8d8b15367b4687c9040f2addd
Дима <dima.kotko@mail.ru>
2023-05-19 16:53:39 +0300

    Потиху делаю кнопки

---
+++ Assets/Scripts/ButtonsController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsController : MonoBehaviour
+{
+
+    public Canvas pauseCanvas;
+
+   public void PauseButton()
+    {
+
+    }
+}
