2ff9f2b3b34d8875c3f9aa40d781d198dae0f585
Yana <yanakot1357@gmail.com>
2023-05-19 21:20:58 +0300

    Исправлен заяц

--- Assets/Scripts/BunnyMovement.cs
+++ Assets/Scripts/BunnyMovement.cs
@@ -7,16 +7,17 @@ public class BunnyMovement : MonoBehaviour
     public float speed;
     private Animator anim;
     private AudioSource audioSource;
-    public AudioClip run;
+    public AudioClip laugh;
     void Start()
     {
         anim = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(laugh);
+        audioSource.Play();
     }
     void FixedUpdate()
     {
         anim.SetBool("isMove", true);
-        //audioSource.PlayOneShot(run);
         transform.Translate(speed / 100.0f, 0, 0);           //Заяц просто ходит с определённой скоростью, можно добавить проверку на финиш потом
     }
 }
