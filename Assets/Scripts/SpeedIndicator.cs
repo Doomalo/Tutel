448e197c97c9a2e127bd38fb1a1ab2ab70d27651
melnidary <melnikavadarya.de@gmail.com>
2023-05-20 13:09:14 +0300

    генератор бустеров максимальная высота текст скорости

--- Assets/Scripts/SpeedIndicator.cs
+++ Assets/Scripts/SpeedIndicator.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SpeedIndicator : MonoBehaviour
 {
     public TurtleMovement turtle;
+    private double speed;
 
     private void FixedUpdate()
     {
         if (turtle)
-            this.gameObject.GetComponent<UnityEngine.UI.Text>().text = turtle.speedX.ToString();
+        {
+            speed = Math.Round(turtle.speedX, 1);
+            this.gameObject.GetComponent<UnityEngine.UI.Text>().text = speed.ToString();
+        }
         else
             this.gameObject.GetComponent<UnityEngine.UI.Text>().text = "0";
     }
